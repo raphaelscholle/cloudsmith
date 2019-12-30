28eac18186109b8604bf57dd14b8cee9eb8da6be
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 20:56:18 -0800

    BB-762 ready to send the command for real

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,24 @@ file=$5
 distro=$6
 release=$7
 
-pip install cloudsmith-cli
+
 # requires a CLOUDSMITH_API_KEY env variable to push
-cloudsmith push $action $format $org/$repo/distro/release some-file.deb
+if [[ -z $CLOUDSMITH_API_KEY ]]; then
+    echo "CLOUDSMITH_API_KEY is required"
+    exit 1
+fi
+
+if [[ "$command" != "push" ]]; then
+    echo "command $comand not yet implemented."
+    exit 3
+fi
+
+pip install cloudsmith-cli
+
+
+if [[ "$format" == "deb" ]]; then
+   cloudsmith push $action $format $org/$repo/$distro/$release $file
+else
+    echo "format $format not yet implemented."
+    exit 2
+fi
