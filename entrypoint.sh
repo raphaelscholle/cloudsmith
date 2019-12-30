ac366a09254de20db34096b5fbac8657474dc032
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 16:08:59 -0800

    master trying most basic script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-command=$1
-format=$2
-org=$3
-repo=$4
-file=$5
-distro=$6
-release=$7
+echo "hello"
+# command=$1
+# format=$2
+# org=$3
+# repo=$4
+# file=$5
+# distro=$6
+# release=$7
 
-apt-get -y install python-pip
-pip install cloudsmith-cli
-# requires a CLOUDSMITH_API_KEY env variable to push
-push $action $format $org/$repo/distro/release some-file.deb
+# apt-get -y install python-pip
+# pip install cloudsmith-cli
+# # requires a CLOUDSMITH_API_KEY env variable to push
+# push $action $format $org/$repo/distro/release some-file.deb
