5dfa956d08f0b3961c1fca389b25b7356370c5b8
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 20:06:03 -0800

    BB-762 back to python 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ file=$5
 distro=$6
 release=$7
 
-apt -y update
-apt -y install python3-pip
-pip3 install cloudsmith-cli
+apt-get -y update
+apt-get -y install python-pip
+pip install cloudsmith-cli
 # requires a CLOUDSMITH_API_KEY env variable to push
 cloudsmith push $action $format $org/$repo/distro/release some-file.deb
