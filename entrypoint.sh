50b655b1f0a75ff2ed88ee9134e867a84e492f5b
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 17:47:15 -0800

    BB-762 upgrading pip isn't necessary

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,6 @@ release=$7
 
 apt-get -y update
 apt-get -y install python-pip
-pip install --upgrade pip
 pip install cloudsmith-cli
 # requires a CLOUDSMITH_API_KEY env variable to push
 cloudsmith push $action $format $org/$repo/distro/release some-file.deb
