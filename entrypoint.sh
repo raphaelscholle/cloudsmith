b5787feeeabee89dc1735f80daaf8678b9947770
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 19:37:55 -0800

    BB-762 trying ubuntu 18

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,6 @@ file=$5
 distro=$6
 release=$7
 
-apt-get -y update
-apt-get -y install python3-pip
 pip3 install cloudsmith-cli
 # requires a CLOUDSMITH_API_KEY env variable to push
 cloudsmith push $action $format $org/$repo/distro/release some-file.deb
