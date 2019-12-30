02d341ee421b850266931b12dd4db5613aae0847
Aaron Roller <aaron.roller@automodality.com>
2019-12-29 18:07:36 -0800

    BB-762 trying pip3

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ distro=$6
 release=$7
 
 apt-get -y update
-apt-get -y install python-pip
-pip install cloudsmith-cli
+apt-get -y install python-pip3
+pip3 install cloudsmith-cli
 # requires a CLOUDSMITH_API_KEY env variable to push
 cloudsmith push $action $format $org/$repo/distro/release some-file.deb
